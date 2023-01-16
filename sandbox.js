d6e3e15ada6354872dcd8ed13d5e0154707c07c8
Ezekiel <eabodesegun@essolo.com>
2023-01-16 19:15:05 +0100

    basic types in typescript

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,1 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var character = "mario";
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,1 @@
-const character = 'mario';
-
-console.log(character);
-
-const inputs = document.querySelectorAll('input');
-
-inputs.forEach(input => {
-  console.log(input);
-});
+let character = "mario";
